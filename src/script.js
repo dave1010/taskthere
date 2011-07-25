78d8b22e6c07f22d00915b16e987bd919e1dc3c9
Dave Hulbert <dave1010@gmail.com>
2011-07-25 22:50:16 +0100

    WIP

--- src/script.js
+++ src/script.js
@@ -1,120 +1,198 @@
-// TODO: make OO & clean up lots
 
-// object hash of place objects
-// places are in the form {name: place_name,lat: 53,lng: -1,tasks: []}
-// tasks are just strings
-var places = {};
+var elements = {
+	placeSelect: $('#p'),
+	taskContainer: $('#t'),
+	taskInput: $('#new') 
+};
 
-function start_edit_place() {
-  $('#edit-place').show();
-  $('#place-selector').hide();
+var placeManager = {
+	getCurrentPlaceName: function() {
+		return elements.placeSelect.val();
+	},	
+	getCurrent: function() {
+		var n = placeManager.getCurrentPlaceName();
+		for (var i in placeManager.places) {
+			if (placeManager.places[i].name == n) {
+				return placeManager.places[i];
+			}
+		}
+	},
+	update: function() {
+		// save the tasks/places & trigger a rebuild of the task list
+		placeManager.saveToStorage();
+		elements.placeSelect.change();
+	},
+	saveToStorage: function() {
+		localStorage.places = JSON.stringify(placeManager.places); 
+	},
+	restoreFromStorage: function() {
+		// TODO: make into Place objects
+		// restore from last session if it exists
+		var places = localStorage.places ? JSON.parse(localStorage.places) : [];
+		for (var i in places) {
+			placeManager.places.push(new Place(places[i]);
+		}
+	},
+	startEditPlace: function() {
+		$('#edit-place').show();
+		$('#place-selector').hide();
 
-  var p = places[place()];
-  $('#place-name').val(p.name);
-  $('#place-lat').val(p.lat);
-  $('#place-lng').val(p.lng);
+		var p = placeManager.getCurrent();
+		$('#place-name').val(p.name);
+		$('#place-lat').val(p.lat);
+		$('#place-lng').val(p.lng);
+	},
+	cancelEditPlace: function() {
+		$('#edit-place').hide();
+		$('#place-selector').show();
+	},
+	saveEditPlace: function() {
+		alert('not implemented');
+		placeManager.cancelEditPlace();
+	},
+	startNewPlace: function() {
+		var place_name = prompt('Place name: ');
+		if (!place_name) {
+			alert('Please enter a place name');
+			return;
+		}
+
+		// TODO: chgeck if place exists
+		var opts = {
+			name: place_name,
+			lat = 53,
+			lng = -1,
+		}
+
+		placeManager.places.push(new Place(opts, tasks));
+		placeManager.saveToStorage();
+		placeManager.buildSelect();
+		elements.placeSelect.val(place_name);
+		placeManager.update();
+	},
+	addTask: function(details) {
+		if (!placeManager.getCurrentName()) {
+			alert('Add a place before adding a task');
+			return;
+		}
+		var t = new Task(details);
+		var p = placeManager.getCurrent();
+		p.addTask(t);
+		placeManager.update();
+		return false;
+	},
+	buildSelect: function() {
+		// TODO: show task count and distance to current location
+		// TODO: sort order: alphabetical or distance
+		var options = '';
+		var place;
+		for (var i in placeManager.places) {
+			place = placeManager.places[i];
+			options += place.getOptionHtml();
+		}
+		options += '<option value="" id="new-place-option">New place&hellip;</option>';
+		elements.placeSelect.html(options);
+	},
+	deleteTask: function() {
+		places[current_place_name()].tasks.splice(i, 1);
+		update();
+	},
+	registerEvents: function() {
+		elements.placeSelect.change(on.placeSelectChange);
+	},
+	// array of place objects
+	// places are in the form {name: place_name,lat: 53,lng: -1,tasks: []}
+	places: []
+};
+
+// event handlers
+var on = {
+	addTask: function() {
+		var details = $('#new').val();
+		placeManager.addTask(details);
+		$('#new').val('');
+		return false;	
+	},
+	placeSelectChange: function() {
+		if (elements.placeSelect.val() == '') {
+			placeManager.startNewPlace();
+			return;
+		}
+		var p = placeManager.getCurrent();
+		p.select();
+		elements.taskInput.focus();
+	}
+};
+
+
+function Place(opts, tasks) {
+	this.name = opts.name;
+	this.lat = opts.lat;
+	this.lng - opts.lng;
+	this.tasks = tasks || [];
 }
 
-function cancel_edit_place() {
-  $('#edit-place').hide();
-  $('#place-selector').show();
+Place.prototype.addTask = function(task) {
+	this.tasks.push(task);
 }
 
-function save_edit_place() {
-  alert('not implemented');
-  cancel_edit_place();
+
+Place.prototype.add = function() {
+};
+
+Place.prototype.startEdit = function() {
+};
+
+Place.prototype.save = function() {
+};
+
+Place.prototype.delete = function() {
+};
+
+Place.prototype.select = function() {
+	var html = '';
+	var tasks = this.tasks;
+	var a, task;
+	elements.taskContainer.html('');	
+	for (var i in tasks) {
+		task = tasks[i];
+		elements.taskContainer.append(task.getHtml());
+	}
+};
+
+Place.prototype.getOptionHtml = function() {
+	// TODO: escape chars
+	return '<option>' + this.name + '</option>';
+};
+
+Place.prototype.addTask = function(task) {
+	this.tasks.push(task);
+};
+
+
+
+function Task(title) {
+	this.title = title;
 }
 
-// start creating a new place
-function start_new_place() {
-  var place_name = prompt('Place name: ');
-  if (!place_name) {
-    alert('Please enter a place name');
-    return;
-  }
-  if (places[place_name]) {
-    alert(place_name + ' already exists');
-  }
-  places[place_name] = {
-    name: place_name,
-    lat: 53,
-    lng: -1,
-    tasks: []
-  };
+Task.prototype.getHtml = function() {
+	var me = this;	
+	return $('<a />')
+		.text(this.title)
+		.click(function() {
+			me.delete();
+		})
+		.wrap('<li />');
+};
 
-  localStorage.places = JSON.stringify(places);
-  build_select();
-  $('#p').val(place_name);
-  update();
-}
+Task.prototype.delete = function() {
+	alert('deleting task ' + this.name);
+	// TODO: delete task
+};
 
-function add_task() {
-  if (!place()) {
-    alert('Add a place before adding a task');
-    return;
-  }
-  places[place()].tasks.push($('#new').val());
-  $('#new').val('');
-  update();
-  return false;
-}
+placeManager.restoreFromStorage();
+placeManager.buildSelect();
+placeManager.registerEvents();
+placeManager.update();
 
-// build the HTML for the places select menu
-function build_select() {
-  // TODO: show task count and distance to current location
-  // TODO: sort order: alphabetical or distance
-  var options = '';
-  for (var i in places) {
-      options += '<option>' + places[i].name + '</option>';
-  }
-  options += '<option value="" id="new-place-option">New place&hellip;</option>';
-  $('#p').html(options);
-}
-
-// get the currently selected place
-function place() {
-    return $('#p').val();
-}
-
-function delete_task(i) {
-  places[place()].tasks.splice(i, 1);
-  update();
-}
-
-// make so changing the place updates the tasks
-function register_events() {
-  $('#p').change(function(){
-    if ($('#p').val() == '') {
-      start_new_place();
-      return;
-    }
-    var p = place();
-    var html = '';
-    var tasks = window.places[p].tasks;
-    var a;
-    for (var i in tasks) {
-      a = $('<p />');
-      a.text(tasks[i]);
-      html += '<li><a href="javascript:void(delete_task(' + i + '))">' + a.text() + '</a></li>';
-    }
-    $('#t').html(html);
-    $('#new').focus();
-  });
-}
-
-// save the tasks/places & trigger a rebuild of the task list
-function update() {
-  localStorage.places = JSON.stringify(places);
-  $('#p').change();
-}
-
-// restore from last session if it exists
-function init() {
-  window.places = localStorage.places ? JSON.parse(localStorage.places) : {};
-}
-
-init();
-build_select();
-register_events();
-update();
 //alert('js ok');
